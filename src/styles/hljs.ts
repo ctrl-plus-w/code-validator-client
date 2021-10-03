6960d572c2ae5c99c128bbdfabccf38e348b8bb6
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-10-03 11:26:17 +0200

    Added ReactSyntaxHighlighter

---
+++ src/styles/hljs.ts
@@ -0,0 +1,101 @@
+const hljsTheme = {
+  'hljs-comment': {
+    color: '#79B8FF',
+  },
+  'hljs-quote': {
+    color: '#9ECBFF',
+  },
+  'hljs-variable': {
+    color: '#cc6666',
+  },
+  'hljs-template-variable': {
+    color: '#cc6666',
+  },
+  'hljs-tag': {
+    color: '#cc6666',
+  },
+  'hljs-name': {
+    color: '#cc6666',
+  },
+  'hljs-selector-id': {
+    color: '#cc6666',
+  },
+  'hljs-selector-class': {
+    color: '#cc6666',
+  },
+  'hljs-regexp': {
+    color: '#cc6666',
+  },
+  'hljs-deletion': {
+    color: '#cc6666',
+  },
+  'hljs-number': {
+    color: '#de935f',
+  },
+  'hljs-built_in': {
+    color: '#de935f',
+  },
+  'hljs-builtin-name': {
+    color: '#de935f',
+  },
+  'hljs-literal': {
+    color: '#de935f',
+  },
+  'hljs-type': {
+    color: '#de935f',
+  },
+  'hljs-params': {
+    color: '#de935f',
+  },
+  'hljs-meta': {
+    color: '#de935f',
+  },
+  'hljs-link': {
+    color: '#de935f',
+  },
+  'hljs-attribute': {
+    color: '#f0c674',
+  },
+  'hljs-string': {
+    color: '#9ECBFF',
+  },
+  'hljs-symbol': {
+    color: '#b5bd68',
+  },
+  'hljs-bullet': {
+    color: '#b5bd68',
+  },
+  'hljs-addition': {
+    color: '#b5bd68',
+  },
+  'hljs-title': {
+    color: '#B392F0',
+  },
+  'hljs-section': {
+    color: '#81a2be',
+  },
+  'hljs-keyword': {
+    color: '#F97583',
+  },
+  'hljs-selector-tag': {
+    color: '#b294bb',
+  },
+  'hljs-operator': {
+    color: 'red',
+  },
+  hljs: {
+    display: 'block',
+    overflowX: 'auto',
+    background: '#24292E',
+    color: '#E1E4E8',
+    padding: '0.5em',
+  },
+  'hljs-emphasis': {
+    fontStyle: 'italic',
+  },
+  'hljs-strong': {
+    fontWeight: 'bold',
+  },
+};
+
+export default hljsTheme;
