ac7108a049baeb501e316d881010a5e14da0d014
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-10-06 19:29:33 +0200

    Added some models

--- src/types/models.d.ts
+++ src/types/models.d.ts
@@ -1,11 +1,32 @@
+interface IRole {
+  name: string;
+  slug: string;
+}
+
+interface IUser {
+  name: string;
+  surname: string;
+  username: string;
+  gender: 'male' | 'female';
+  role?: IRole;
+}
+
+interface IEvaluationUser extends IUser {
+  returned: Date | null;
+  note: number | null;
+  status: 'waiting' | 'done' | 'todo';
+}
+
 interface IEvaluation {
   title: string;
   description: string;
   deadline: Date;
   totalUsers: number;
   completedUsers: number;
+  maxNote: numbers;
   group: {
     name: string;
     slug: string;
   };
+  users: IEvaluationUser[];
 }
