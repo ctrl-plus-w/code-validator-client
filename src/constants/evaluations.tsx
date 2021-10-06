240c9d007adf600d0aa4ce4fe898238806e03004
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-10-06 19:30:03 +0200

    Added some evaluations and some users fake data

--- src/constants/evaluations.tsx
+++ src/constants/evaluations.tsx
@@ -1,22 +1,55 @@
-const evaluations = [
+import { lukas, robinson, rose } from './users';
+
+const evaluations: Array<IEvaluation> = [
   {
     title: 'Afficher la note',
-    description: `Un élève a obtenu une note N. Afficher:
-  − « Bravo c'est extraordinaire » s'il a obtenu plus de 19
-  − « vraiment très bien » si la note est comprise entre 16 et 19
-  − « c'est bien » si la note est entre 12 et 16
-  − « pas mal » si la note est entre 10 et 12
-  − « encore un effort » si la note est comprise entre 8 et 10
-  − « du travail est nécessaire » si la note est entre 5 et 8
-  − « insuffisant » sinon
+    description: `Un élève a obtenu une note N. Afficher:  
+− « Bravo c'est extraordinaire » s'il a obtenu plus de 19  
+− « vraiment très bien » si la note est comprise entre 16 et 19  
+− « c'est bien » si la note est entre 12 et 16  
+− « pas mal » si la note est entre 10 et 12  
+− « encore un effort » si la note est comprise entre 8 et 10  
+− « du travail est nécessaire » si la note est entre 5 et 8  
+− « insuffisant » sinon  
+  
+\`\`\`python
+# Voici la syntaxe de la fonction 
+# La fonction doit retourner une chaîne de charactères
+# et prends en paramètres un nombre entier
+
+def message_note(nb):
+  pass
+
+\`\`\`
    `,
     deadline: new Date(2021, 9, 13),
     totalUsers: 32,
     completedUsers: 32,
+    maxNote: 15,
     group: {
       name: 'NSI Terminale',
       slug: 'nsi-terminale',
     },
+    users: [
+      {
+        ...lukas,
+        returned: new Date(2021, 9, 11, 15, 30),
+        note: null,
+        status: 'todo',
+      },
+      {
+        ...robinson,
+        returned: new Date(2021, 9, 11, 6, 25),
+        note: 15,
+        status: 'done',
+      },
+      {
+        ...rose,
+        returned: null,
+        note: null,
+        status: 'waiting',
+      },
+    ],
   },
   {
     title: 'Faire une facture',
@@ -32,10 +65,31 @@ Total TTC           20%              1.74 €`,
     deadline: new Date(2021, 9, 20),
     totalUsers: 32,
     completedUsers: 0,
+    maxNote: 15,
     group: {
       name: 'NSI Terminale',
       slug: 'nsi-terminale',
     },
+    users: [
+      {
+        ...lukas,
+        returned: new Date(2021, 9, 11, 15, 30),
+        note: null,
+        status: 'todo',
+      },
+      {
+        ...robinson,
+        returned: new Date(2021, 9, 11, 6, 25),
+        note: 15,
+        status: 'done',
+      },
+      {
+        ...rose,
+        returned: null,
+        note: null,
+        status: 'waiting',
+      },
+    ],
   },
   {
     title: 'Afficher les entiers ',
@@ -44,10 +98,31 @@ b) Écrire un programme qui affiche les entiers impairs de 20 à 0`,
     deadline: new Date(2021, 9, 17),
     totalUsers: 32,
     completedUsers: 12,
+    maxNote: 15,
     group: {
       name: 'NSI Terminale',
       slug: 'nsi-terminale',
     },
+    users: [
+      {
+        ...lukas,
+        returned: new Date(2021, 9, 11, 15, 30),
+        note: null,
+        status: 'todo',
+      },
+      {
+        ...robinson,
+        returned: new Date(2021, 9, 11, 6, 25),
+        note: 15,
+        status: 'done',
+      },
+      {
+        ...rose,
+        returned: null,
+        note: null,
+        status: 'waiting',
+      },
+    ],
   },
 ];
 
---
+++ src/constants/users.tsx
@@ -0,0 +1,31 @@
+export const lukas: IUser = {
+  name: 'Lukas',
+  surname: 'Laudrain',
+  username: 'llaudrain',
+  gender: 'male',
+};
+
+export const rose: IUser = {
+  name: 'Rose',
+  surname: 'Yazid',
+  username: 'ryazid',
+  gender: 'female',
+};
+
+export const emma: IUser = {
+  name: 'Emma',
+  surname: 'Cadro',
+  username: 'ecadro',
+  gender: 'female',
+};
+
+export const robinson: IUser = {
+  name: 'Robinson',
+  surname: 'Rambeau',
+  username: 'rrambeau',
+  gender: 'male',
+};
+
+const users = [lukas, rose, robinson, emma];
+
+export default users;
