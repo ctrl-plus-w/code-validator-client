c828650cd9906a7ff6e2f5051dd7c3ba76071312
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-11-28 20:45:44 +0100

    Added a success style for the button skeleton

--- src/components/skeletons/ButtonSkeleton.tsx
+++ src/components/skeletons/ButtonSkeleton.tsx
@@ -4,18 +4,20 @@ import clsx from 'clsx';
 
 interface IProps {
   small?: boolean;
-  primary?: boolean;
   className?: string;
+
+  primary?: boolean;
+  success?: boolean;
 }
 
-const ButtonSkeleton = ({ small, primary, className }: IProps): ReactElement => {
+const ButtonSkeleton = ({ small, primary, success, className }: IProps): ReactElement => {
   return (
     <div
       className={clsx([
         'px-6 py-2.5 rounded',
-        primary
-          ? 'from-primary bg-gradient-to-br to-purple-300'
-          : 'from-gray-600 bg-gradient-to-br to-gray-300',
+        primary && 'from-primary bg-gradient-to-br to-purple-300',
+        success && 'from-green-800 bg-gradient-to-br to-green-300',
+        !primary && !success && 'from-gray-600 bg-gradient-to-br to-gray-300',
         small ? 'h-10' : 'h-10.5',
         className,
       ])}
