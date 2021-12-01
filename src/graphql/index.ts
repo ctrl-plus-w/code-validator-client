de3acf7ba26d506d7aa05ed87c452633775aca61
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-12-01 20:36:51 +0100

    Removed the server uri assertion

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,8 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const { NEXT_PUBLIC_SERVER_URI } = process.env;
 
-if (!NEXT_PUBLIC_SERVER_URI) throw new Error('Server uri not defined');
-
 const client = new ApolloClient({
   uri: `${NEXT_PUBLIC_SERVER_URI}/graphql`,
   cache: new InMemoryCache(),
